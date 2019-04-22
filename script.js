07c3b9a1873dc8c38582e5c09a9761883f10b33d
Filipp <fis_@mail.ru>
2019-04-22 21:27:23 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'),
+    circle = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart'), //селектор чтобы получить через класс ставим .
+    oneHeart = document.querySelector('.heart');
+
+console.log(btn[1]);
+console.log(btn);
+console.log(circle[2]);
+console.log(heart);
+console.log(oneHeart);
